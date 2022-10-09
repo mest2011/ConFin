a0f87a68b097f48579570129af59144cf46e49d6
mest2011 <mest2011mest@gmail.com>
2022-10-09 18:29:47 -0300

    fix: align itens on lateral menu

--- app/view/imports/menu_lateral.php
+++ app/view/imports/menu_lateral.php
@@ -26,34 +26,34 @@ if (!$log->salvarLog($_SERVER["REQUEST_URI"])) {
             <img class="menu-icon my-auto" src="./images/menu-gray.png" alt="https://www.freepik.com/">
         </div>
         <div class="menu-expand mobile-off">
-            <div class="d-flex py-4 px-5 selectable-none align-items-baseline mobile-off">
-                <h3 class="font-white">Cofrin</h3>
+            <div class="d-flex pt-4 selectable-none align-items-baseline mobile-off align-menu-icons">
+                <h4 class="font-white mb-0">Cofrin</h4>
                 <img class="logo" src="./images/logo_porquinho.png" alt="Logo do porquinho">
             </div>
         </div>
 
         <ul class="d-block mobile-margin-menu">
-            <a class="font-gray d-flex py-4 px-5 menu-opcoes mobile-off" href="../view/dashboard.php" title="Ir para dashboard">
+            <a class="font-gray d-flex py-4 align-menu-icons menu-opcoes mobile-off" href="../view/dashboard.php" title="Ir para dashboard">
                 <img class="menu-icon my-auto menu-expand-img" src="./images/home.png" alt="home">
                 <img class="menu-icon my-auto menu-expand-invert-img" src="./images/home-gray.png" alt="home">
                 <li class="menu-expand ml-3 mr-5 my-auto">Home</li>
             </a>
-            <a class="font-gray d-flex py-4 px-5 menu-opcoes" href="../view/ganhos.php" title="Ver todos os ganhos do mês">
+            <a class="font-gray d-flex py-4 align-menu-icons menu-opcoes" href="../view/ganhos.php" title="Ver todos os ganhos do mês">
                 <img class="menu-icon my-auto my-auto menu-expand-img" src="./images/rendimento.png" alt="ganhos">
                 <img class="menu-icon my-auto menu-expand-invert-img" src="./images/rendimento-gray.png" alt="ganhos">
                 <li class="menu-expand ml-3 mr-5 my-auto">Meus ganhos</li>
             </a>
-            <a class="font-gray d-flex py-4 px-5 menu-opcoes" href="../view/gastos.php" title="Ver todas as despesas do mês">
+            <a class="font-gray d-flex py-4 align-menu-icons menu-opcoes" href="../view/gastos.php" title="Ver todas as despesas do mês">
                 <img class="menu-icon my-auto my-auto menu-expand-img" src="./images/calendario.png" alt="gastos">
                 <img class="menu-icon my-auto menu-expand-invert-img" src="./images/calendario-gray.png" alt="gastos">
                 <li class="menu-expand ml-3 mr-5 my-auto">Minhas despesas</li>
             </a>
-            <a class="font-gray d-flex py-4 px-5 menu-opcoes" href="../view/extrato.php" title="Ver extrato do mês">
+            <a class="font-gray d-flex py-4 align-menu-icons menu-opcoes" href="../view/extrato.php" title="Ver extrato do mês">
                 <img class="menu-icon my-auto my-auto menu-expand-img" src="./images/extrato.png" alt="extrato">
                 <img class="menu-icon my-auto menu-expand-invert-img" src="./images/extrato-gray.png" alt="extarto">
                 <li class="menu-expand ml-3 mr-5 my-auto">Extrato</li>
             </a>
-            <a class="font-gray d-flex py-4 px-5 menu-opcoes" href="../view/carteiras.php" title="Ver todas as carteiras cadastradas!">
+            <a class="font-gray d-flex py-4 align-menu-icons menu-opcoes" href="../view/carteiras.php" title="Ver todas as carteiras cadastradas!">
                 <img class="font-gray menu-icon my-auto my-auto menu-expand-img" src="./images/carteira.png" alt="carteira">
                 <img class="font-gray menu-icon my-auto menu-expand-invert-img" src="./images/carteira-gray.png" alt="carteira">
                 <li class="menu-expand ml-3 mr-5 my-auto">Carteiras</li>
